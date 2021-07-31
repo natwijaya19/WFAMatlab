fd4c0097340b40a6c59499f4e4480dea4c3470a7
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2021-07-31 22:27:24 +0700

    Upload file

---
+++ runWFA1.m
@@ -0,0 +1,23 @@
+% Copyright 2016 - 2017 The MathWorks, Inc.
+%% Start WFA for overall optimization
+%Setup parameters for WFA
+setParam;
+for i = 1:nWalk
+    disp('=============== In-sample test ===============')
+    disp(wfaPeriod(i,:))
+    dateIST2 = dateIST{i};
+    
+    % ===== In-sample test =====
+    pnl = zeros(1,nParam);
+    sharpeRatio = zeros(1,nParam);
+    parfor j = 1:nParam
+        [pnl(j),sharpeRatio(j)] = runIST(u,prices,dateIST2,param(j));
+    end
+    resultTable = table(param',pnl',sharpeRatio');
+    optimParam = calcOptimParam(resultTable,paramType);
+    
+    % ===== Out-of-sample test =====
+    [pnlTable,P,T,TOut,TClose] = runOST(u,prices,pnlTable,P,...
+        T, TOut, TClose, dateOST{i}, optimParam);
+end
+plotSummary;
